ee759fdbbfc82079a49653eb2f111672a5d48540
Gabriel Arboleda <gabrielarboleda2000@hotmail.com>
2024-04-15 21:46:26 -0500

    Se separaron las funciones de cada archivo, ya que estas pueden ser usadas en el desarrollo de diferentes casos

---
+++ functions.py
@@ -0,0 +1,57 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+from tqdm import tqdm
+
+#Function to wait for the page to load
+def waitPageLoad(message, timeout):
+    seconds = range(timeout)
+    for second in tqdm(seconds, desc=message, bar_format="{l_bar}{bar}|"):
+        time.sleep(0.1)
+
+#Wait for the AdBlock extension to be activated
+def waitAdblockActivation(driver):
+    waitPageLoad("Esperando que cargue la extensión de AdBlock...", 80)
+    print("Se cargó la extensión de AdBlock\n")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    driver.refresh()
+
+#Check the product details and extract only the necessary information
+def checkProductDetails(element, index, PRODUCTDETAILSLABELS, PRODUCTDETAILS, PRODUCTDETAILSSUCCESSCASE, PRODUCTDETAILSFAILURECASE):
+    labelTitle = PRODUCTDETAILSLABELS[index]
+    newElement = ""
+    lenLabelWords = len(PRODUCTDETAILS[labelTitle].split(" "))
+    countWords = 1
+    for productDetailsWord in PRODUCTDETAILS[labelTitle].split(" "):
+        for elementWord in element.split(" "):
+            if(elementWord == productDetailsWord):
+                if(countWords == lenLabelWords):
+                    newElement += elementWord
+                else:
+                    newElement += elementWord + " "
+                countWords += 1
+                break
+    if(PRODUCTDETAILS[labelTitle] == newElement):
+        print(PRODUCTDETAILSSUCCESSCASE[labelTitle])
+    else:
+        print(PRODUCTDETAILSFAILURECASE[labelTitle], newElement)
+        raise AssertionError
+    
+#Function to check if an element is visible
+def checkIfElementIsVisible(element, XPATHMessage, driver, XPATHSUCCESSCASE, XPATHFAILURECASE):
+    if(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, element)))):
+        print(XPATHSUCCESSCASE[XPATHMessage])
+    else:
+        print(XPATHFAILURECASE[XPATHMessage])
+        raise NoSuchElementException
+    
+def checkText(element, text, driver, XPATHSUCCESSCASE, XPATHFAILURECASE, XPATHMessage):
+    if(driver.find_element(By.XPATH, element).text == text):
+        print(XPATHSUCCESSCASE[XPATHMessage])
+    else:
+        print(XPATHFAILURECASE[XPATHMessage])
+        raise AssertionError
